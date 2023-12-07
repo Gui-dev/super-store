9ca83814bd3c178f78b57a328d3f93b214550ca8
Gui Silva <adrianon10@gmail.com>
2023-12-07 04:08:43 -0300

    chore: add next auth config

---
+++ src/@types/env.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DATABASE_URL: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
+    HOST_URL: string
+    STRIPE_WEBHOOK_SECRET_KEY: string
+    NEXT_PUBLIC_STRIPE_KEY: string
+  }
+}
