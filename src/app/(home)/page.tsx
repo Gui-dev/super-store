246b8fb15b4486fe831b9e53c9d7ceaea2baffce
Gui Silva <adrianon10@gmail.com>
2023-12-08 04:52:20 -0300

    feat: add first discount banner from home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 50% de Descontos só esse mês"
+      />
+    </main>
+  )
+}
