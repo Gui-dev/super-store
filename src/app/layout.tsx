b1e860f08cb007c0216553bdf04d435720cb3a25
Gui Silva <adrianon10@gmail.com>
2023-12-08 03:31:59 -0300

    feat: add auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 
+import { AuthProvider } from '@/providers/auth'
 import { Header } from '@/components/ui/header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,8 +17,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
