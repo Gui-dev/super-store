e5160618690c8bab10afb02b47df7b81733cdae2
Gui Silva <adrianon10@gmail.com>
2023-12-18 04:38:37 -0300

    feat: add header menu component

---
+++ src/components/ui/header-menu.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import { HomeIcon, ListOrderedIcon, MenuIcon, PercentIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from './sheet'
+
+import { AuthButton } from './auth-button'
+import { UserAvatar } from './user-avatar'
+import { Button } from './button'
+
+export const HeaderMenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <h1 className="text-left text-sm font-semibold">Menu</h1>
+        </SheetHeader>
+
+        <UserAvatar />
+
+        <div className="mt-4 flex flex-col gap-2">
+          <SheetClose asChild>
+            <Link
+              href="/"
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <HomeIcon size={16} />
+              Home
+            </Link>
+          </SheetClose>
+          <SheetClose asChild>
+            <Link
+              href="/offers"
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <PercentIcon size={16} />
+              Ofertas
+            </Link>
+          </SheetClose>
+          <SheetClose asChild>
+            <Link
+              href="/catalog"
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Link>
+          </SheetClose>
+          <AuthButton />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,72 +1,13 @@
-import {
-  HomeIcon,
-  ListOrderedIcon,
-  MenuIcon,
-  PercentIcon,
-  ShoppingCartIcon,
-} from 'lucide-react'
-
-import { Button } from './button'
-import { Card } from './card'
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTrigger,
-} from './sheet'
-import { AuthButton } from './auth-button'
-import { UserAvatar } from './user-avatar'
 import Link from 'next/link'
+
+import { Card } from './card'
+import { HeaderMenu } from './header-menu'
+import { CartMenu } from './cart-menu'
 
 export const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <h1 className="text-left text-sm font-semibold">Menu</h1>
-          </SheetHeader>
-
-          <UserAvatar />
-
-          <div className="mt-4 flex flex-col gap-2">
-            <SheetClose asChild>
-              <Link
-                href="/"
-                className="flex w-full items-center justify-start gap-2"
-              >
-                <HomeIcon size={16} />
-                Home
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link
-                href="/offers"
-                className="flex w-full items-center justify-start gap-2"
-              >
-                <PercentIcon size={16} />
-                Ofertas
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link
-                href="/catalog"
-                className="flex w-full items-center justify-start gap-2"
-              >
-                <ListOrderedIcon size={16} />
-                Catálogo
-              </Link>
-            </SheetClose>
-            <AuthButton />
-          </div>
-        </SheetContent>
-      </Sheet>
+      <HeaderMenu />
 
       <Link href="/">
         <h1 className="text-lg font-semibold">
@@ -74,9 +15,7 @@ export const Header = () => {
         </h1>
       </Link>
 
-      <Button size="icon" variant="outline">
-        <ShoppingCartIcon />
-      </Button>
+      <CartMenu />
     </Card>
   )
 }
