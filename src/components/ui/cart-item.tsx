97f0771f2acf52e32a967ada6605c049921c5052
Gui Silva <adrianon10@gmail.com>
2023-12-19 05:03:07 -0300

    feat: add cart item component

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,19 +1,63 @@
 import { ICartProduct } from '@/providers/cart'
+import Image from 'next/image'
+import { Button } from './button'
+import { MinusIcon, PlusIcon, TrashIcon } from 'lucide-react'
 
 interface ICartItem {
-  products: ICartProduct[]
+  product: ICartProduct
 }
 
-export const CartItem = ({ products }: ICartItem) => {
+export const CartItem = ({ product }: ICartItem) => {
   return (
-    <div className="flex">
-      {products.map((product) => {
-        return (
-          <div key={product.id} className="flex">
-            <h1>{product.name}</h1>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <div className="flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.image_urls[0]}
+            alt={product.name}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-xs font-bold">{product.name}</p>
+          <div className="flex items-center gap-2">
+            {product.discount_percent > 0 && (
+              <>
+                <p className="text-sm font-bold">
+                  R$ {product.total_price.toFixed(2)}
+                </p>
+                <p className="text-xs line-through opacity-75">
+                  R$ {Number(product.base_price).toFixed(2)}
+                </p>
+              </>
+            )}
+            {product.discount_percent === 0 && (
+              <p className="text-sm font-bold">
+                R$ {Number(product.base_price).toFixed(2)}
+              </p>
+            )}
           </div>
-        )
-      })}
+
+          <div className="flex items-center gap-2">
+            <Button size="icon" variant="outline" className="h-7 w-7">
+              <MinusIcon size={16} />
+            </Button>
+            <span className="text-xs">{product.quantity}</span>
+            <Button size="icon" variant="outline" className="h-7 w-7">
+              <PlusIcon size={16} />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <Button size="icon" variant="outline" className="h-7 w-7">
+          <TrashIcon size={16} />
+        </Button>
+      </div>
     </div>
   )
 }
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,14 +2,17 @@
 
 import { useCart } from '@/hooks/use-cart'
 import { CartItem } from './cart-item'
+import { computeProductTotalPrice } from '@/helpers/product'
 
 export const Cart = () => {
   const { products } = useCart()
 
   return (
-    <div className="flex">
-      <h1>CART</h1>
-      <CartItem products={products} />
+    <div className="mt-8 flex flex-col gap-5">
+      {products.map((product) => {
+        const product_final = computeProductTotalPrice({ product }) as any
+        return <CartItem key={product.id} product={product_final} />
+      })}
     </div>
   )
 }
