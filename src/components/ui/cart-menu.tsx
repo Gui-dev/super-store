f09d70b96fa5cd006ac2b48a6ed2d1b18fd3aad2
Gui Silva <adrianon10@gmail.com>
2023-12-19 03:44:55 -0300

    feat: add cart menu component

---
+++ src/components/ui/cart-menu.tsx
@@ -0,0 +1,30 @@
+import { ShoppingCartIcon } from 'lucide-react'
+
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
+import { Button } from './button'
+import { Cart } from './cart'
+import { Badge } from './badge'
+
+export const CartMenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline">
+          <ShoppingCartIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right">
+        <SheetHeader>
+          <Badge
+            className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem]"
+            variant="outline"
+          >
+            <ShoppingCartIcon size={16} />
+            <h1 className="text-base uppercase">Carrinho</h1>
+          </Badge>
+        </SheetHeader>
+        <Cart />
+      </SheetContent>
+    </Sheet>
+  )
+}
